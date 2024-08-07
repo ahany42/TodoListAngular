51faed7de7bd9822528a0b51c20986811a905af1
ahany42 <ahanyfathy42@gmail.com>
2024-08-07 14:44:39 +0200

    bootsrap navbar ,intial home screen

--- myapp/src/app/Components/home/home.component.ts
+++ myapp/src/app/Components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StoreData } from 'src/app/ViewModels/store-data';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+storeInfo:StoreData;
+isImgShown:boolean=true;
+constructor(){
+  this.storeInfo=new StoreData("Test Store","https://picsum.photos/50/50",["New Cairo","Nasr City"]);
+}
+toggleImg(){
+  this.isImgShown=!this.isImgShown;
+}
 
 }
---
+++ myapp/src/app/ViewModels/store-data.spec.ts
@@ -0,0 +1,7 @@
+import { StoreData } from './store-data';
+
+describe('StoreData', () => {
+  it('should create an instance', () => {
+    expect(new StoreData()).toBeTruthy();
+  });
+});
